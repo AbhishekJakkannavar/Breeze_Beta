8c9f13d0371ac195bcb8e56c8a478dcfad0eaeb6
Abhishek Jakkannavar <tracerabhi96@gmail.com>
2020-01-23 19:57:14 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/app/com/nextandco/breezeapp_beta/MainActivity.kt
@@ -0,0 +1,13 @@
+package app.com.nextandco.breezeapp_beta
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
