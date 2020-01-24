1258dc0e7c1e4ab698dac0d94855a1fa791f9292
renuvkelkar <renuvkelkar@gmail.com>
2020-01-24 10:30:31 +0000

    Home page UI with all files

---
+++ lib/design.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class design extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title:  Text('Programming'),
+
+        backgroundColor: Color(0xFFffbb00),
+        elevation: 0.0,
+
+
+      ),
+    );
+  }
+}
